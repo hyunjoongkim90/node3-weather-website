184ec0e8e04677768ad943632043da4c2ac069be
Hyunjoong Kim <hyunjoongkim90@gmail.com>
2022-04-22 23:57:48 +0900

    add humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Invalid coordinates. Try again.', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. 현재 온도는 ${Math.round((body.current.temperature - 32) * 5/9)} ℃, 체감온도는 ${Math.round((body.current.feelslike -32) * 5/9)} ℃ 입니다.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. 
+            현재 온도는 ${Math.round((body.current.temperature - 32) * 5/9)} ℃, 
+            체감온도는 ${Math.round((body.current.feelslike -32) * 5/9)} ℃ 입니다. 
+            현재 습도는 ${body.current.humidity}%입니다.`)
         }
     })
     
